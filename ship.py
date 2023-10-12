69cda96926fec180e9d8f774a1825d77ef2d72a9
Abishek Subedi <sabitakiran602@gmail.com>
2023-10-12 16:21:27 -0400

    Added alien in the game

--- ship.py
+++ ship.py
@@ -8,7 +8,7 @@ class Ship:
         self.ai_settings = ai_settings
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load("images/ship3.png")
+        self.image = pygame.image.load("images/spaceship_dark.png")
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
