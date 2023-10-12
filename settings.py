3fcc877e453cd2e55f5657489d4b8b2005c4640a
Abishek Subedi <sabitakiran602@gmail.com>
2023-10-12 14:44:01 -0400

    first phase done (base and fire)

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings:
         """Initilize the game settings."""
         # Screen Settings
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
 
         # Ship settings
@@ -16,3 +16,4 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
