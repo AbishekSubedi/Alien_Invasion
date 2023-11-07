1b7cd56abf734129b47d5304afb53a14c1709924
Abishek Subedi <sabitakiran602@gmail.com>
2023-11-07 14:44:21 -0500

    Added ship limit and game ending feature

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings:
         """Initilize the game settings."""
         # Screen Settings
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
 
         # Ship settings
