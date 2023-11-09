2b5e0123d68dbf31bea0debb7e01ecc33797a734
Abishek Subedi <sabitakiran602@gmail.com>
2023-11-09 15:56:42 -0500

    added level ups

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings:
     """'A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """Initilize the game settings."""
+        """Initilize the game's static settings."""
         # Screen Settings
         self.screen_width = 1200
         self.screen_height = 600
@@ -22,5 +22,23 @@ class Settings:
         # Alien settings
         self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game"""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
