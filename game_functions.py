e3d986f6612f95440cedb88c16f419b5d92e9296
Abishek Subedi <sabitakiran602@gmail.com>
2023-11-09 16:26:43 -0500

    created scoreboard for the game

--- game_functions.py
+++ game_functions.py
@@ -119,7 +119,7 @@ def check_play_button(
         ship.center_ship()
 
 
-def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
+def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
@@ -130,6 +130,9 @@ def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button
 
     ship.blitme()
     aliens.draw(screen)
+
+    # Draw the score information.
+    sb.show_score()
 
     # Draw the play button if the game is inactive.
     if not stats.game_active:
