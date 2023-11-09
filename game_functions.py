ee7d370fb1d7ddb3533bf9e8b48945792b457bbb
Abishek Subedi <sabitakiran602@gmail.com>
2023-11-09 15:39:14 -0500

    deactivating the play button and hiding cursor

--- game_functions.py
+++ game_functions.py
@@ -52,6 +52,8 @@ def check_keydown_events(event, screen, ship, ai_settings, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
 
 
 def fire_bullet(ai_settings, screen, ship, bullets):
@@ -73,9 +75,7 @@ def check_keyup_events(event, ship):
 def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:
             check_keydown_events(event, screen, ship, ai_settings, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
@@ -98,7 +98,11 @@ def check_play_button(
     ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y
 ):
     """Start a new game when the player clicks Play."""
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
+        # Hide the mouse cursor.
+        pygame.mouse.set_visible(False)
+
         # Reset the game statistics.
         stats.reset_stats()
         stats.game_active = True
@@ -189,6 +193,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         sleep(0.5)
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
