907f51e6159b2b25927a654a59c795e5fb7ad99c
Abishek Subedi <sabitakiran602@gmail.com>
2023-12-18 00:54:51 -0500

    Final product (scoreboard and levels added)

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings:
         """Initilize the game's static settings."""
         # Screen Settings
         self.screen_width = 1200
-        self.screen_height = 600
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
         # Ship settings
@@ -25,6 +25,8 @@ class Settings:
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -34,11 +36,15 @@ class Settings:
         self.bullet_speed_factor = 3
         self.alien_speed_factor = 1
 
+        self.alien_points = 50
+
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
